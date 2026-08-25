78d7e8cbf0d42fa2681ebcb08a726a2d6f21619b
mdsajidsh631-dotcom <mdsajidsh631@gmail.com>
2026-08-25 06:48:55 +0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// Quiz Master - Firebase Configuration
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB1MgiAd4PH4j1_GdzHXhg2cYzTNlOHFLI",
+    authDomain: "quiz-master-53557.firebaseapp.com",
+    projectId: "quiz-master-53557",
+    storageBucket: "quiz-master-53557.firebasestorage.app",
+    messagingSenderId: "596300534675",
+    appId: "1:596300534675:web:df41f72543b6e4a3d64e66"
+};
