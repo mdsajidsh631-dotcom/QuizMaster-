bb1787dd09ae41eccf7a8bf7b72b3283eb14c19d
mdsajidsh631-dotcom <mdsajidsh631@gmail.com>
2026-08-25 06:47:07 +0600

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/*
+ * Quiz Master
+ * Authentication Controller
+ * Step 1
+ */
+
+// Google Login
+function loginWithGoogle() {
+    alert(
+        "Google Login এখনো সংযুক্ত করা হয়নি। " +
+        "পরবর্তী Authentication ধাপে Firebase/Google Authentication যুক্ত করা হবে।"
+    );
+}
+
+// Account Recovery
+function showMessage() {
+    alert(
+        "Account Recovery সিস্টেম পরবর্তী ধাপে সংযুক্ত করা হবে।"
+    );
+}
+
+// Back to Home
+function goHome() {
+    window.location.href = "../index.html";
+}
